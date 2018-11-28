67661cd62a267fb3f6d46bfd295c2e2dee6abbb2
Gabriel Matos <gab.matos@outlook.com>
2018-11-28 17:00:26 +0000

    Tested for dock

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,12 @@
 class DockingStation
   def release_bike
+    Bike.new
+  end
+  def dock(bike)
   end
 end
 class Bike
   def working?
+    true
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,11 @@ require 'docking_station'
 
   describe DockingStation do
     it { is_expected.to respond_to :release_bike}
-end
+
+    it "gets a working bike" do
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
+
+    it { is_expected.to respond_to(:dock).with(1).argument}
+  end
